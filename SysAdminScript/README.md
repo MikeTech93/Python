30c0f3c3b60c43775c98a9411ef66f82877d4c5c
Mike Etherington <mike@CentOS-Test-002.localhost>
2020-08-18 10:25:49 -0400

    Updated readme.md and cli.py

--- SysAdminScript/README.md
+++ SysAdminScript/README.md
@@ -1,10 +1,12 @@
 ### Description 
 
-This script will perform a menu with a choice of tools for linux administrators
+This script will perform a menu with a choice of tools for linux administrators.
+
+Each menu option will have a return to main menu and exit option for easy navigation.
 
 ### Usage 
 
-1. File Permissions#
+1. File Permissions
 	1. Perform basic ls command
 		> This will prompt for a specified path and then perform an ls
 	2. Advanced permissions review
@@ -37,26 +39,81 @@ This script will perform a menu with a choice of tools for linux administrators
 
 3. File\folder Managmeent
 	1. Create file/folder
+		> This will prompt if you want to create a file or folder
+		> Then where exactly you would like to create it
 	2. Rename file/folder
+		> This will prompt for a file/folder 
+		> Then ask you what you would like it to be renamed too
 	3. Copy file/folder
+		> This will prompt you for a file/folder
+		> If a folder is selected it will ask if you want to recursively copy
+		> Then it will ask where you want to copy it to
 	4. Move file/folder
+		> This will prompt you for a file/folder
+		> then it will ask where you want to move it to
 	5. Delete file/folder
+		> this will prompt you for a file/folder
+		> then it will delete the file/folder
 
 4. Display System Information
-	Computer Name
-	Operating System
-	Host
-	Kernel Version
-	Uptime
-	CPU
-	GPU
-	RAM
+	1. Display System information
+		> This will display a list of system information:
+			Hostname
+			Operating System
+			Linux Kernel and Version
+			Uptime
+			CPU
+			GPU
+			RAM	
 
 5. System Maintenance
-	1. Top 5 CPU processes
-	2. Top 5 RAM Processes
-	3. Top 5 Size folders
-	4. Manage logs
+	1. Top 5 processes by CPU%
+		> This will display a list of Top 5 processes consuming processing power
+	2. Top 5 processes by MEM%
+		> This will display a list of Top 5 processes consuming RAM
+	3. Top 5 folders by size on disk
+		> This will display the largest 5 folders on the current running system.
+	4. Launch HTOP
+		> This will check if HTOP is installed and if not it will prompt you to install it
+		> It will then launch HTOP
+	5. Manage logs
+		> This will prompt for a log folder location - It will give some common locations as examples
+		> It will then search for log files in this destination and present these
+		> This will then prompt for confirmation and compress all log files into a tar.gz file
+
+6. User Management
+	1. List Users
+		1. Regular users
+			> List all users who can login to a shell
+		2. Root Users
+			> List all users with root access
+		3. Service accounts
+			> List all service accounts
+		4. All accounts
+			> List all accounts
+	2. Create User
+		> This will prompt for a username
+		> Ater checking the user does not already exist it will ask for a few details
+		> Before creating the user it will display all values you entered and ask for confirmation
+		> If enter is pressed it will keep the current value
+	3. Edit User
+		> This will prompt for a username
+		> Ater matching the user it will loop through a few of the users details presenting the current set value and asking if the user would like to change this value
+		> If enter is pressed it will keep the current value
+	4. Change User Password
+		> Prompt for a username
+		> Ask if you want a randomly generated password or a custom password
+	5. Delete User
+		> Prompt for a username
+		> Ask if you would like to do anything with the users home folder
+		> if yes - prompt if they would like to delete or change ownership of the folder
+
+7. Group Management
+	1. List groups
+	2. Create Group
+	3. Edit Group
+	4. Change group password
+	5. Delete group
 
 ### Examples
 
--- SysAdminScript/src/SysAdminTools/cli.py
+++ SysAdminScript/src/SysAdminTools/cli.py
@@ -17,7 +17,9 @@ def mainmenu():
 3. File/Folder Management
 4. Display System Information
 5. System Maintenance
-6. Exit
+6. User Management
+7. Group Management
+8. Exit
 
 Please enter your choice: """)
 
@@ -32,9 +34,13 @@ Please enter your choice: """)
     elif choice == "5":
         systemmaintenance()
     elif choice == "6":
+        UserManagement()
+    elif choice == "7":
+        GroupManagement()
+    elif choice == "8":
         sys.exit
     else:
-        print("You must only select either 1,2,3,4,5 or 6")
+        print("You must only select either 1,2,3,4,5,6,7 or 8")
         print("Please try again")
         mainmenu()
 
@@ -64,16 +70,218 @@ Please enter your choice: """)
         filepermissions()
 
 def networksettings():
-    print("You selected the Network Settings option")
+    print("************NETWORK SETTINGS MENU**************")
+    #time.sleep(1)
+    print()
+    choice = input("""
+1: View Network Settings
+2. Configure Network Settings
+3. Return to main menu
+4. Exit 
+
+Please enter your choice: """)
+
+    if choice == "1":
+        print("You selected to View Network Settings")
+    elif choice == "2":
+        print("You selected to Configure Network Settings")
+    elif choice == "3":
+        mainmenu()
+    elif choice == "4":
+        sys.exit
+    else:
+        print("You must only select either 1,2,3 or 4")
+        print("Please try again")
+        networksettings()
 
 def filefoldermanagement():
-    print("You selected the File/Folder Management option")
+    print("************File\Folder Management**************")
+    #time.sleep(1)
+    print()
+    choice = input("""
+1: Create file/folder
+2: Rename file/folder
+3. Copy file/folder
+4. Move file/folder
+5. Delete file/folder
+6. Return to main menu
+7. Exit
+
+Please enter your choice: """)
+
+    if choice == "1":
+        print("You selected to Create file/folder")
+    elif choice == "2":
+        print("You selected to Rename file/folder")
+    elif choice == "3":
+        print("You selected to Copy file/folder")
+    elif choice == "4":
+        print("You selected to Move file/folder")
+    elif choice == "5":
+        print("You selected to Delete file/folder")
+    elif choice == "6":
+        mainmenu()
+    elif choice == "7":
+        sys.exit
+    else:
+        print("You must only select either 1,2,3,4,5,6 or 7")
+        print("Please try again")
+        filefoldermanagement()
 
 def systeminformation():
-    print("You have selected the System Information option")
+    print("************NETWORK SETTINGS MENU**************")
+    #time.sleep(1)
+    print()
+    choice = input("""
+1: Display System Information
+2. Return to main menu
+3. Exit 
+
+Please enter your choice: """)
+
+    if choice == "1":
+        print("You selected to Display System Information")
+    elif choice == "2":
+        mainmenu()
+    elif choice == "3":
+        sys.exit
+    else:
+        print("You must only select either 1,2 or 3")
+        print("Please try again")
+        systeminformation()
 
 def systemmaintenance():
-    print(f"You have selected the System Maintenance option")
+    print("************System Maintenance**************")
+    #time.sleep(1)
+    print()
+    choice = input("""
+1: Top 5 Processes by CPU%
+2: Top 5 Processes by MEM%
+3. Top 5 folders by size on disk
+4. Launch HTOP
+5. Manage Logs
+6. Return to main menu
+7. Exit
+
+Please enter your choice: """)
+
+    if choice == "1":
+        print("You selected to view top 5 processes by CPU%")
+    elif choice == "2":
+        print("You selected to view top 5 processes by MEM%")
+    elif choice == "3":
+        print("You selected to view top 5 folders by size on disk")
+    elif choice == "4":
+        print("You selected to launch HTOP")
+    elif choice == "5":
+        print("You selected to manage logs")
+    elif choice == "6":
+        mainmenu()
+    elif choice == "7":
+        sys.exit
+    else:
+        print("You must only select either 1,2,3,4,5,6 or 7")
+        print("Please try again")
+        systemmaintenance()
+
+def UserManagement():
+    print("************User Management**************")
+    #time.sleep(1)
+    print()
+    choice = input("""
+1: List Users
+2: Create User
+3. Edit User
+4. Change User Password
+5. Delete User
+6. Return to main menu
+7. Exit
+
+Please enter your choice: """)
+
+    if choice == "1":
+        UserManagementListSubMenu()
+    elif choice == "2":
+        print("You have selected to create a new user")
+    elif choice == "3":
+        print("You have selected to Edit a user")
+    elif choice == "4":
+        print("You have selected to change a user password")
+    elif choice == "5":
+        print("You have selected to delete a user")
+    elif choice == "6":
+        mainmenu()
+    elif choice == "7":
+        sys.exit
+    else:
+        print("You must only select either 1,2,3,4,5,6 or 7")
+        print("Please try again")
+        UserManagement()
+
+def UserManagementListSubMenu():
+    print("************List Users**************")
+    #time.sleep(1)
+    print()
+    choice = input("""
+1: Regular Users
+2: Root Users
+3. Service Accounts
+4. All Accounts
+5. Return to main menu
+6. Exit
+
+Please enter your choice: """)
+
+    if choice == "1":
+        print("You have selected to list all regular users")
+    elif choice == "2":
+        print("You have selected to list root users")
+    elif choice == "3":
+        print("You have selected to list all service accounts")
+    elif choice == "4":
+        print("You have selected to list all accounts")
+    elif choice == "5":
+        mainmenu()
+    elif choice == "6":
+        sys.exit
+    else:
+        print("You must only select either 1,2,3,4,5 or 6")
+        print("Please try again")
+        UserManagementListSubMenu()
+
+def GroupManagement():
+    print("************Group Management**************")
+    #time.sleep(1)
+    print()
+    choice = input("""
+1: List Groups
+2: Create Group
+3. Edit Group
+4. Change Group Password
+5. Delete Group
+6. Return to main menu
+7. Exit
+
+Please enter your choice: """)
+
+    if choice == "1":
+        print("You have selected to list Groups")
+    elif choice == "2":
+        print("You have selected to create a new Group")
+    elif choice == "3":
+        print("You have selected to Edit a Group")
+    elif choice == "4":
+        print("You have selected to change a Groups password")
+    elif choice == "5":
+        print("You have selected to delete a Group")
+    elif choice == "6":
+        mainmenu()
+    elif choice == "7":
+        sys.exit
+    else:
+        print("You must only select either 1,2,3,4,5,6 or 7")
+        print("Please try again")
+        GroupManagement()
 
 
 # function definitions end
